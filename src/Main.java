8bce1a64b470f60149597bbe8ba03c2ab23a0f94
asdf8911 <87693377+asdf8911@users.noreply.github.com>
2022-05-11 10:10:31 +0300

    bmi is renamed to bodyMassIndex

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(54, 1.75);
-        System.out.printf("%.2f", bmi);
+        double bodyMassIndex = service.calculate(54, 1.75);
+        System.out.printf("%.2f", bodyMassIndex);
     }
 }
