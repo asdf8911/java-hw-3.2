cbfa2a6d0b52dd059ffea70a8fb4195661b7a8e1
Ekaterina <vipolsovatel@mail.ru>
2022-05-15 14:03:52 +0300

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double m, double h) {
-        double index = m / Math.pow(h, 2);
-        return index;
+    public double calculate(double mass, double height) {
+        double bodyMassIndex = mass / Math.pow(height, 2);
+        return bodyMassIndex;
     }
 }
